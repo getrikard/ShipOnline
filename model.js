de290143bb1a2ae6a324d29a80408d4ffa7d7ac6
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2019-11-27 14:16:13 +0100

    Modelen
    Dette er en beskrivelse som sier at modelen er modelen.

---
+++ model.js
@@ -0,0 +1,74 @@
+let model = {
+    username: "OleIversen",
+    currentCompany: "Color Line",
+    companies: [
+        {
+            isFavorite: true,
+            name: "",
+            adresses: [
+                {
+                    type: "visiting",
+                    adress: ""
+                },
+                {
+                    type: "postbox",
+                    adress: ""
+                }
+            ],
+            place: "",
+            country: "NOR",
+            groups: [],
+            communications: [
+                {
+                    type: "phone",
+                    number: 000,
+                    department: "fmsdfs"
+                }
+            ],
+            departments: [
+                "fdsf",
+                "fsdfss"
+            ],
+            categories: [
+                "fsdgfdfg",
+                "asdsf"
+            ],
+            companyNo: [
+                {
+                    type: "IMO",
+                    number: 00
+                },
+                {
+                    type: "Enterprise",
+                    number: 0345
+                }
+            ]
+        }
+    ],
+    contacts: [
+        {
+            name: "",
+            company: "",
+            stilling: "",
+            department: "",
+            privateAdress: "",
+            tlf: 0,
+            picture: "img.png"
+        }
+    ],
+    fleet: [
+        {
+            isFavorite: true,
+            name: "fsd",
+            GT: 123,
+            MDWT: 11,
+            built: 1999,
+            callsign: "SKO",
+            fishID: "V-555",
+            flag: "NOR",
+            role: "MO",
+            company: "fgghh",
+            type: "fishing"
+        }
+    ]
+}
