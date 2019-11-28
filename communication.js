e1e415aea56277c8c748a35978373118a1040d66
GET Rikard <rikard@getacademy.no>
2019-11-28 12:22:32 +0100

    Update communication.js

--- communication.js
+++ communication.js
@@ -1,3 +1,20 @@
 function communication(params) {
+    let html = `<div>
+        <a href="javascript:addCommunication">Add communication</a>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <th></th>
+            </tr>
+        </table>
+    </div>`;
+}
+
+function getShowCommunicationHTML() {
+
+}
+
+function getAddCommunicationHTML() {
 
 }
