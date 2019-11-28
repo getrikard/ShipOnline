b5a9c4fecf130a947932012e67b44c5178fc29c8
trinityn <52109867+trinityn@users.noreply.github.com>
2019-11-28 12:41:01 +0100

    la til scrpt src

--- controllerJS/company.js
+++ controllerJS/company.js
@@ -2,4 +2,5 @@
 const getCompany = () => {
     console.log('Hello there, this page is under maintenance');
     //content.innerHTML = model.view.currentContent;
+    // model.data.companies[0].name
 }
