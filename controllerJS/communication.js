0ecd8e1e60f86f8adf977042227ea3d9182e8a18
GET Rikard <rikard@getacademy.no>
2019-11-28 15:30:18 +0100

    Update communication.js

--- controllerJS/communication.js
+++ controllerJS/communication.js
@@ -1,18 +1,13 @@
 function communication(params) {
     let html = `<div>
         <a href="javascript:addCommunication">Add communication</a>
-    </div>
-    <div>
-        <table>
-            <tr>
-                <th></th>
-            </tr>
-        </table>
     </div>`;
 }
 
 function getShowCommunicationHTML() {
-
+    let html = `<div>
+        
+    </div>`;
 }
 
 function getAddCommunicationHTML() {
