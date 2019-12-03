620e0cd3c21c0639ede1e821d3f4c1aec78bfc5f
trinityn <52109867+trinityn@users.noreply.github.com>
2019-12-03 11:21:50 +0100

    Update company.js

--- controllerJS/company.js
+++ controllerJS/company.js
@@ -1,8 +1,8 @@
 // JavaScript source code
 const getCompanyPage = () => {
-    let htmlCompany = `<p>${model.data.username} is the name. ${model.data.username}</p>
+    let htmlCompany = `<p>${model.data.username} is the name.
                        ${model.data.currentCompanyID} is the company ID
-                       ${model.data.companies[0].adresses[1].type}`;
+                       ${model.data.companies[0].adresses[1].type}</p>`;
     model.view.currentContent = htmlCompany;
     show();
 }
