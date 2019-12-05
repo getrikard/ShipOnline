dc44ca845fad0aafaaadb2877d877b52ba6f87ea
GET Rikard <rikard@getacademy.no>
2019-12-05 11:22:20 +0100

    Nå kan man legge til selskaper

--- js/pages/showAddCompany.js
+++ js/pages/showAddCompany.js
@@ -1,3 +1,71 @@
 function showAddCompany() {
+    let html = `
+        <table>
+            <tr>
+                <td>
+                    Name:
+                </td>
+                <td>
+                    <input type="text" id="addNameInput">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Category:
+                </td>
+                <td>
+                    <input type="text" id="addCategoryInput">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Place:
+                </td>
+                <td>
+                    <input type="text" id="addPlaceInput">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Country:
+                </td>
+                <td>
+                    <select id="addCountryInput">
+                        <option>NOR</option>
+                        <option>SWE</option>
+                        <option>DNK</option>
+                        <option>GER</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
 
+        <button onclick="cancelCompanyToModel()">Cancel</button>
+        <button onclick="addCompanyToModel()">Save</button>
+    `;
+
+    model.view.mainContent = html;
+    show();
+}
+
+function addCompanyToModel() {
+    const name = document.getElementById('addNameInput').value;
+    const categories = document.getElementById('addCategoryInput').value.split(' ');
+    const place = document.getElementById('addPlaceInput').value;
+    const country = document.getElementById('addCountryInput').value;
+    const company = {
+        isFavorite: false,
+        name: name,
+        categories: categories,
+        place: place,
+        country: country
+    };
+    model.data.companies.push(company);
+    showCompanies();
+    show();
+}
+
+function cancelCompanyToModel() {
+    showCompanies();
+    show();
 }
