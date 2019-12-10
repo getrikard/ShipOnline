ac1c92933a1dd0d344f5f9f83cb21a208b117842
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2019-12-10 13:57:33 +0100

    YIKES

--- js/pages/showLandingpage.js
+++ js/pages/showLandingpage.js
@@ -1,7 +1,11 @@
 function showLandingPage() {
     model.view.mainContent = `  <div id="content">
     <div class="mainContentWrapper">
-        <div> Here we'll display the big buttons </div>
+    <div class="LandingPage">
+    <div id="companiesIcon"><img src="IMG/company.png" alt="picture"></div>
+    <div id="shipsIcon"><img src="IMG/Ship icon blue.png" alt="picture"></div>
+    <div id="contractIcon"><img src="IMG/contract.png" alt="picture"></div>
+</div>
         </div>
 </div>`;
     show();
