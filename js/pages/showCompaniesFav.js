7578724703db279f8a57d2c5161223447273771b
trinityn <52109867+trinityn@users.noreply.github.com>
2019-12-10 10:40:25 +0100

    added showCompaniesFav.js

--- js/pages/showCompaniesFav.js
+++ js/pages/showCompaniesFav.js
@@ -1,5 +1,6 @@
-﻿function showCompanies() {
-    let htmlCompanies =
+﻿// JavaScript source code
+function showFavCompanies() {
+    let htmlCompaniesFav =
         ` <div id="content"> <div class="topLineInContent">
         <span id="companyButton"><b>SP</b>SHIPBASE Company ➡ </span>
         <a href="javascript:showAddCompany()" id="addCompanyButton">Add Company</a>
@@ -15,19 +16,19 @@
   </tr>`;
     for (let i = 0; i < model.data.companies.length; i++) {
         let company = model.data.companies[i];
-        htmlCompanies += `
+        if (company.isFavorite == true) {
+            htmlCompaniesFav += `
         <tr>
         <td onclick="showCompany(${i}); console.log(${i})" class ="clickAble">${company.name}</td>
         <td>${company.categories}</td>
         <td>${company.place}</td>
         <td>${company.country}</td>
-        <td onclick="showFavCompanies()">${company.isFavorite ? '⭐' : ''}</td>
+        <td onclick="showCompanies()">${company.isFavorite ? '⭐' : ''}</td>
         <td></td>
   </tr>`;
-    };
+        };
 
-    htmlCompanies += `</table> </div>`;
-
-    model.view.mainContent = htmlCompanies;
-    show();
+        model.view.mainContent = htmlCompaniesFav;
+        show();
+    }
 };
