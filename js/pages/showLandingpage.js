96dfc4410d021dc75b432b63908a594946a5da6a
GET Rikard <rikard@getacademy.no>
2019-12-10 14:42:36 +0100

    Landing buttons centered and fixes

--- js/pages/showLandingpage.js
+++ js/pages/showLandingpage.js
@@ -1,11 +1,10 @@
 function showLandingPage() {
     model.view.mainContent = `  <div id="content">
     <div class="LandingPage">
-    <div id="companiesIcon" onclick="showCompanies()"><img src="IMG/company.png" alt="picture"></div>
-    <div id="shipsIcon" onclick="showShipsList()"><img src="IMG/Ship icon blue.png" alt="picture"></div>
-    <div id="contractIcon" onclick="showContactsList()"><img  src="IMG/contract.png" alt="picture"></div>
+    <div class="landingbutton" onclick="showCompanies()"><img src="IMG/company.png" alt="picture"></div>
+    <div class="landingbutton" onclick="showShipsList()"><img src="IMG/Ship icon blue.png" alt="picture"></div>
+    <div class="landingbutton" onclick="showContactsList()"><img  src="IMG/contract.png" alt="picture"></div>
 </div>
-        </div>
 </div>`;
     show();
 }
