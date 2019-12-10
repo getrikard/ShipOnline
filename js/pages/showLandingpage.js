0ab13cd2598286ad9959145be6630d9fced265bf
trinityn <52109867+trinityn@users.noreply.github.com>
2019-12-10 13:58:53 +0100

    Update showLandingpage.js

--- js/pages/showLandingpage.js
+++ js/pages/showLandingpage.js
@@ -1,6 +1,5 @@
 function showLandingPage() {
     model.view.mainContent = `  <div id="content">
-    <div class="mainContentWrapper">
     <div class="LandingPage">
     <div id="companiesIcon"><img src="IMG/company.png" alt="picture"></div>
     <div id="shipsIcon"><img src="IMG/Ship icon blue.png" alt="picture"></div>
