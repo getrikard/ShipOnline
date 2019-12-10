200963d1566ae1735c1b88b59473e984e999216c
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2019-12-10 14:59:34 +0100

    Jala

--- js/pages/showLandingpage.js
+++ js/pages/showLandingpage.js
@@ -3,7 +3,7 @@ function showLandingPage() {
     <div class="LandingPage">
     <div class="landingbutton" onclick="showCompanies()"><img src="IMG/company.png" alt="picture"></div>
     <div class="landingbutton" onclick="showShipsList()"><img src="IMG/Ship icon blue.png" alt="picture"></div>
-    <div class="landingbutton" onclick="showContactsList()"><img  src="IMG/contract.png" alt="picture"></div>
+    <div class="landingbutton" onclick="showContactsList()"><img  src="IMG/contact.png" alt="picture"></div>
 </div>
 </div>`;
     show();
