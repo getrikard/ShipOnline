e31f9d7e94524b55e379abf4944528b0ebb0c94c
trinityn <52109867+trinityn@users.noreply.github.com>
2019-12-10 13:40:10 +0100

    added showShipsList() & showContactsList

---
+++ js/pages/showContactsList.js
@@ -0,0 +1,33 @@
+﻿function showContactsList() {
+    let htmlContacts =
+        ` <div id="content"> <div class="topLineInContent">
+        <span id="companyButton"><b>SP</b>SHIPBASE Company ➡ </span>
+        <a href="javascript:showAddCompany()">Add Company</a>
+        </div>
+    <table>
+  <tr>
+    <th>Name</th>
+    <th>Relation</th>
+    <th>Number</th>
+    <th>Email</th>
+    <th class ="myCompanyTab" onclick="showFavCompanies()">VIP</th>
+    <th>Groups</th>
+  </tr>`;
+    for (let i = 0; i < model.data.contacts.length; i++) {
+        let contacts = model.data.contacts[i];
+        htmlContacts += `
+        <tr>
+        <td class ="clickAble">Elvis Presley</td>
+        <td>Wife</td>
+        <td>113</td>
+        <td>Elvispresley@gmail.com</td>
+        <td>⭐</td>
+        <td>Intern</td>
+  </tr>`;
+    };
+
+    htmlContacts += `</table> </div>`;
+
+    model.view.mainContent = htmlContacts;
+    show();
+};
---
+++ js/pages/showShipsList.js
@@ -0,0 +1,33 @@
+﻿function showShipsList() {
+    let htmlShips =
+        ` <div id="content"> <div class="topLineInContent">
+        <span id="companyButton"><b>SP</b>SHIPBASE Company ➡ </span>
+        <a href="javascript:showAddCompany()">Add Company</a>
+        </div>
+    <table>
+  <tr>
+    <th>Name of ship</th>
+    <th>Type</th>
+    <th>Location</th>
+    <th>Fishing ID</th>
+    <th class ="myCompanyTab" onclick="showFavCompanies()">My Ship</th>
+    <th>Groups</th>
+  </tr>`;
+    for (let i = 0; i < model.data.companies.length; i++) {
+        let company = model.data.companies[i];
+        htmlShips += `
+        <tr>
+        <td onclick="showCompany(${i}); console.log(${i})" class ="clickAble">${company.name}</td>
+        <td>${company.categories}</td>
+        <td>${company.place}</td>
+        <td>${company.country}</td>
+        <td>${company.isFavorite ? '⭐' : ''}</td>
+        <td></td>
+  </tr>`;
+    };
+
+    htmlShips += `</table> </div>`;
+
+    model.view.mainContent = htmlShips;
+    show();
+};
