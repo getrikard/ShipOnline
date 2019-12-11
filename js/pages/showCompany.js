2a07987f13b00818dd9513eff7975b0f7adc4fbd
hansget <55439361+hansget@users.noreply.github.com>
2019-12-11 10:11:48 +0100

    Update showCompany.js

--- js/pages/showCompany.js
+++ js/pages/showCompany.js
@@ -1,52 +1,55 @@
 ﻿function showCompany() {
-	let htmlShowCompany = `
-		<div id="leftSide">
-		<div class ="active" onclick="showCompany()">Company</div>
-		<div>Address</div>
-		<div onclick="communication()">Communication</div>
-		<div>Contacts</div>
-		<div>Category</div>
-		<div>Company ID no</div>
-		<div>Documents/Notes</div>
-		<div>Fleet</div>
-		<div>History</div>
-		</div>
-		<div id="content">
+    let htmlShowCompany = `
+            <div id="leftSide">
+                <div class ="active" onclick="showCompany()">Company</div>
+                <div>Address</div>
+                <div onclick="communication()">Communication</div>
+                <div>Contacts</div>
+                <div>Category</div>
+                <div>Company ID no</div>
+                <div>Documents/Notes</div>
+                <div>Fleet</div>
+                <div>History</div>
+            </div>
+            
+            <div id="content">
+            	<table>
+					<tr>
+				    	<th> company name  <br> NOR</th>
+				    </tr>
 
-		<table>
-  <tr>
-    <th> company name  <br> NOR</th>
+				  <tr>
+				    <td> a </td>
+				    <td> a </td>
+				  </tr>
+				  <tr>
+				    <td> b </td>
+				    <td> b </td>
+				  </tr>
+				  <tr>
+				    <td> c  </td>
+				    <td> c  </td>
+				  </tr>
+				  <tr>
+				    <td> d </td>
+				    <td> d </td>
+				  </tr>
+				  <tr>
+				    <td> e </td>
+				    <td> e </td>
+				  </tr>
+				</table>
 
-  </tr>
+            <div>
 
-  <tr>
-    <td> a </td>
-    <td> a </td>
-  </tr>
-  <tr>
-    <td> b </td>
-    <td> b </td>
-  </tr>
-  <tr>
-    <td> c  </td>
-    <td> c  </td>
-  </tr>
-  <tr>
-    <td> d </td>
-    <td> d </td>
-  </tr>
-  <tr>
-    <td> e </td>
-    <td> e </td>
-  </tr>
-</table>
-		
 
-		</div>
-		</div>`;
 
-	model.view.mainContent = htmlShowCompany;
-	show();
+
+
+          	`;
+
+    let companyData = model.data.companies;
+    model.view.mainContentIsGrid = true;
+    model.view.mainContent = htmlShowCompany;
+    show();
 }
-
-
