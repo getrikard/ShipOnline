d33d2bafc8898a3f668ba9698c64074293aa2d18
GET Rikard <rikard@getacademy.no>
2019-12-11 12:38:54 +0100

    pointer og text på landingpage

--- js/pages/showLandingpage.js
+++ js/pages/showLandingpage.js
@@ -3,9 +3,21 @@ function showLandingPage() {
     model.view.showTopNavigation = false;
     model.view.mainContent = `<div id="content">
         <div class="LandingPage">
-            <div class="landingbutton" onclick="showCompanies()"><img src="IMG/company.png" alt="picture"></div>
-            <div class="landingbutton" onclick="showShipsList()"><img src="IMG/Ship icon blue.png" alt="picture"></div>
-            <div class="landingbutton" onclick="showContactsList()"><img  src="IMG/contact.png" alt="picture"></div>
+            <div class="landingbutton" onclick="showCompanies()">
+                <img src="IMG/company.png" alt="picture">
+                <br>
+                <strong>Companies</strong>
+            </div>
+            <div class="landingbutton" onclick="showShipsList()">
+                <img src="IMG/Ship icon blue.png" alt="picture">
+                <br>
+                <strong>Ships</strong>
+            </div>
+            <div class="landingbutton" onclick="showContactsList()">
+                <img  src="IMG/contact.png" alt="picture">
+                <br>
+                <strong>Contacts</strong>
+            </div>
         </div>
     </div>`;
     show();
