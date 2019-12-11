0321ca9b495fd4e922e03ca5e17b230ffe2ec748
hansget <55439361+hansget@users.noreply.github.com>
2019-12-11 09:58:28 +0100

    Update showCompany.js

--- js/pages/showCompany.js
+++ js/pages/showCompany.js
@@ -1,28 +1,50 @@
 ﻿function showCompany() {
-    let htmlShowCompany = `
-            <div id="leftSide">
-                <div class ="active" onclick="showCompany()">Company</div>
-                <div>Address</div>
-                <div onclick="communication()">Communication</div>
-                <div>Contacts</div>
-                <div>Category</div>
-                <div>Company ID no</div>
-                <div>Documents/Notes</div>
-                <div>Fleet</div>
-                <div>History</div>
-            </div>
-            <div id="content">
+	let htmlShowCompany = `
+		<div id="leftSide">
+		<div class ="active" onclick="showCompany()">Company</div>
+		<div>Address</div>
+		<div onclick="communication()">Communication</div>
+		<div>Contacts</div>
+		<div>Category</div>
+		<div>Company ID no</div>
+		<div>Documents/Notes</div>
+		<div>Fleet</div>
+		<div>History</div>
+		</div>
+		<div id="content">
 
-            <div>    </div>
-    <table>
+		<div>    </div>
+		<table>
   <tr>
-  <td> s </td>
+    <th> company name <br> NOR</th>  </tr>
+  <tr>
+    <td>a </td>
+    <td>a </td>
+  </tr>
+  <tr>
+    <td> b </td>
+    <td> b </td>
+  </tr>
+  <tr>
+    <td> c  </td>
+    <td> c  </td>
+  </tr>
+  <tr>
+    <td> d </td>
+    <td> d </td>
+  </tr>
+  <tr>
+    <td> e </td>
+    <td> e </td>
+  </tr>
+</table>
+		
 
-   </div>
-            </div>`;
+		</div>
+		</div>`;
 
-    model.view.mainContent = htmlShowCompany;
-    show();
+	model.view.mainContent = htmlShowCompany;
+	show();
 }
 
 
