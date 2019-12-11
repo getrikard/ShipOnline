bede79da3ba4d801e120d288a9e4f6ac5f337ce9
GET Rikard <rikard@getacademy.no>
2019-12-11 11:13:51 +0100

    liten endring i showcompany(id)

--- js/pages/showCompany.js
+++ js/pages/showCompany.js
@@ -3,39 +3,42 @@ let chosenCompany;
 
 function showCompany(element) {
 	let htmlShowCompany = `
-            ${getLeftBarHTML()}
-            
-            <div id="content">       
-	            	<table>
-						<tr>
-					    	<th> ${companyData.companies[element].name} <br> ${companyData.companies[element].country} </th>
-					    </tr>
+			${getLeftBarHTML()}
+			
+            <div id="content">
+				<table>
+					<tr>
+						<th colspan="2">${companyData.companies[element].name}</th>
+					</tr>
+					<tr>
+						<th colspan="2">${companyData.companies[element].country} </th>
+					</tr>
 
-					  <tr>
-					    <td> visit adress </td>
-					    <td>
-					    	${companyData.companies[element].adresses[0].adress}  
-					    	${companyData.companies[element].adresses[0].postal}
-					    	${companyData.companies[element].adresses[0].place}
-					     </td>
-					  </tr>
-					  <tr>
-					    <td> P.O BOX </td>
-					    <td> b </td>
-					  </tr>
-					  <tr>
-					    <td> Invoice  </td> 
-					    <td> c  </td>
-					  </tr>
-					  <tr>
-					    <td> Phone </td>
-					    <td> d </td>
-					  </tr>
-					  <tr>
-					    <td> Email </td>
-					    <td> e </td>
-					  </tr>
-					</table>
+					<tr>
+					<td> visit adress </td>
+					<td>
+						${companyData.companies[element].adresses[0].adress}  
+						${companyData.companies[element].adresses[0].postal}
+						${companyData.companies[element].adresses[0].place}
+						</td>
+					</tr>
+					<tr>
+					<td> P.O BOX </td>
+					<td> b </td>
+					</tr>
+					<tr>
+					<td> Invoice  </td> 
+					<td> c  </td>
+					</tr>
+					<tr>
+					<td> Phone </td>
+					<td> d </td>
+					</tr>
+					<tr>
+					<td> Email </td>
+					<td> e </td>
+					</tr>
+				</table>
             <div>
 
 
