c62cf5a335c398d162e5722b447b86233a0fb323
trinityn <52109867+trinityn@users.noreply.github.com>
2019-12-12 12:16:32 +0100

    La til showCommunication.js

---
+++ js/pages/showCommunication.js
@@ -0,0 +1,52 @@
+﻿function showCommunication() {
+    let showCommunicationHTML = `
+			${getLeftBarHTML()}
+			
+            <div id="content">
+        <span id="companyButton""><b>SP</b>SHIPBASE Company ➡ </span>
+        <a href="javascript:showAddCompany()">Add Company</a>
+				<table>
+					<tr>
+						<th>${companyData.companies[model.data.currentCompanyID].name}<th>
+					</tr>
+					<tr>
+						<th>${companyData.companies[model.data.currentCompanyID].country}<th>
+					</tr>
+                </table>
+                <div class="addressType">Visiting address</div><br>
+                <table>
+					<tr>
+					<td>Address here</td>
+					</tr>
+					<tr>
+					<td>Address here</td>
+					</tr>
+					<tr>
+					<td>Address here</td> 
+					</tr>
+					<tr>
+					<td>Postal Code: 4020</td>
+					<td>Place: Anywhere</td>
+					</tr>
+					<tr>
+					<td>Country/State:</td>
+					</tr>
+                    <tr>
+                    <td>Nation: Norway</td>
+                    </tr>
+				</table>
+
+            <div class="changeAddNewBottomBar">
+                <span>Web</span>
+                <span>Change</span>
+                <span>Save</span>
+                <span>Add New</span>
+                <span>Cancel</span>
+            </div>
+          	`;
+
+
+        model.view.mainContentIsGrid = true;
+    model.view.mainContent = showCommunicationHTML;
+        show();
+}
