29a1312d019257040b09a45f0596e8a695521d06
GET Rikard <rikard@getacademy.no>
2019-12-12 13:03:40 +0100

    Update showCommunication.js

--- js/pages/leftMenu/showCommunication.js
+++ js/pages/leftMenu/showCommunication.js
@@ -1,22 +1,22 @@
 ﻿function showCommunication() {
     let showCommunicationHTML = `
-			${getLeftBarHTML()}
-			
-            <div id="content">
-        <span id="companyButton""><b>SP</b>SHIPBASE Company ➡ </span>
-        <a href="javascript:showAddCompany()">Add Company</a>
-				<table>
-					<tr>
-						<th>${companyData.companies[model.data.currentCompanyID].name}<th>
-					</tr>
-					<tr>
-						<th>${companyData.companies[model.data.currentCompanyID].country}<th>
-					</tr>
-                </table>
-                <table>
-					<tr>
-                    </tr>
-				</table>
+        ${getLeftBarHTML()}
+        
+        <div id="content">
+            <span id="companyButton""><b>SP</b>SHIPBASE Company ➡ </span>
+            <a href="javascript:showAddCompany()">Add Company</a>
+            <table>
+                <tr>
+                    <th>${companyData.companies[model.data.currentCompanyID].name}<th>
+                </tr>
+                <tr>
+                    <th>${companyData.companies[model.data.currentCompanyID].country}<th>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                </tr>
+            </table>
 
             <div class="changeAddNewBottomBar">
                 <span>Web</span>
@@ -24,11 +24,9 @@
                 <span>Save</span>
                 <span>Add New</span>
                 <span>Cancel</span>
-            </div>
-          	`;
+            </div>`;
 
-
-        model.view.mainContentIsGrid = true;
+    model.view.mainContentIsGrid = true;
     model.view.mainContent = showCommunicationHTML;
-        show();
+    show();
 }
