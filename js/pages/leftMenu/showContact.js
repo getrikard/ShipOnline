4d6bd63ae1fe1de2fb2d9e0bcd3490528f5b73c2
Torjus Tveten <55440508+Torjustveten@users.noreply.github.com>
2019-12-17 12:00:59 +0100

    Table info Contacts

--- js/pages/leftMenu/showContact.js
+++ js/pages/leftMenu/showContact.js
@@ -1,10 +1,10 @@
 ﻿function showContact() {
-        let showContactHTML = `
+    let showContactHTML = `
 			${getLeftBarHTML()}
 			
             <div id="content">
         <span id="companyButton""><b>SP</b>SHIPBASE Company ➡ </span>
-        <a href="javascript:showAddCompany()">Add Company</a>
+        <a href="javascript:showAddCompany()">Add Contact</a>
 				<table>
 					<tr>
 						<th>${companyData.companies[model.data.currentCompanyID].name}<th>
@@ -14,7 +14,50 @@
 					</tr>
                 </table>
                 <table>
-					<tr>
+                    <tr>
+                    <td>Martin Andersen</td>
+                    <td>Company:</td>
+                    </tr>
+                    <tr>
+                    <td>Communication:</td>
+                    </tr>
+                    <tr>
+                    <td>Sharing:</td>
+                    <td>Groups:</td>
+                    </tr>
+                    <tr>
+                    <td>Private Adress:</td>
+                    <td>Place:</td>
+                    </tr>
+                    <tr>
+                    <td>Private Communication:</td>
+                    </tr>
+                    <tr>
+                    <td>Education:</td>
+                    </tr>
+                    <tr>
+                    <td>Sertificate:</td>
+                    </tr>
+                    <tr>
+                    <td>Member:</td>
+                    </tr>
+                    <tr>
+                    <td>Vessels:</td>
+                    </tr>
+                    <tr>
+                    <td>Next of kind:</td>
+                    </tr>
+                    <tr>
+                    <td>Notes / Documents:</td>
+                    </tr>
+                    <tr>
+                    <td>Images:</td>
+                    </tr>
+                    <tr>
+                    <td>Calender:</td>
+                    </tr>
+                    <tr>
+                    <td>History:</td>
                     </tr>
 				</table>
 
@@ -28,7 +71,7 @@
           	`;
 
 
-        model.view.mainContentIsGrid = true;
-        model.view.mainContent = showContactHTML;
-        show();
+    model.view.mainContentIsGrid = true;
+    model.view.mainContent = showContactHTML;
+    show();
 }
