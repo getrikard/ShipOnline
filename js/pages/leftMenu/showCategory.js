f396ea77baeefb14399307f58bb6f0245075dd38
trinityn <52109867+trinityn@users.noreply.github.com>
2019-12-17 12:42:35 +0100

    Update showCategory.js

--- js/pages/leftMenu/showCategory.js
+++ js/pages/leftMenu/showCategory.js
@@ -1,13 +1,24 @@
 function showCategory() {
     let htmlShowCategory = `
-			${getLeftBarHTML()}
-            <div id="content">
-                ${getCompanyButton()} 
-                <td>CATEGORY CONTENT</td>`;
+    ${ getLeftBarHTML() }
+    <div id="content">
+        ${getCompanyButton()}
+        <tr>
+            <th>${companyData.companies[model.data.currentCompanyID].name}<th>
+					</tr>
+                <tr>
+                    <th>${companyData.companies[model.data.currentCompanyID].country}<th>
+					</tr>
+                        <table> <td><b>Category:</b></td><tr>`;
 
-    htmlShowCategory += ` </table> ${getInnerChangeOrSave()} ${getInnerAddOrSave()} ${getInnerFooter()}</div>`;
-
-    model.view.mainContentIsGrid = true;
-    model.view.mainContent = htmlShowCategory;
-    show();
+    let categoryOfCompany = model.data.companies[model.data.currentCompanyID].categories;
+                categoryOfCompany.map((num) => {
+                 htmlShowCategory += `
+                        <td>${num}</td>`;
+                });
+    htmlShowCategory += `</tr> </table> ${getInnerChangeOrSave()} ${getInnerAddOrSave()} ${getInnerFooter()}</div>`;
+                
+model.view.mainContentIsGrid = true;
+model.view.mainContent = htmlShowCategory;
+show();
 }
