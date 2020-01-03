0bc31da32548356558f8978b9cd7b2d971ef4c84
GET Rikard <rikard@getacademy.no>
2020-01-03 10:38:08 +0100

    Loginside første versjon

--- js/pages/showLandingpage.js
+++ js/pages/showLandingpage.js
@@ -1,4 +1,13 @@
 function showLandingPage() {
+    if (model.data.username === undefined) {
+        showLoginPage();
+    }
+    else {
+        showRealLandingPage();
+    }
+}
+
+function showRealLandingPage() {
     model.view.mainContent = `<div id="content">
         <div class="LandingPage">
             <div class="landingbutton" onclick="showCompanies()">
@@ -24,4 +33,39 @@ function showLandingPage() {
     model.view.showLeftNavigation = false;
 
     show();
+}
+
+function showLoginPage() {
+    let html = `
+        <div id="content">
+            <div class="LandingPage" style="padding:50px;">
+                <label>Username: <input id="login-username" type="text"></label>
+                <label>Password: <input id="login-passwd" type="password"></label>
+                <button onclick="login()">Log in</button>
+            </div>
+        </div>
+    `;
+
+    model.view.mainContent = html;
+
+    model.view.mainContentIsGrid = false;
+    model.view.showTopNavigation = false;
+    model.view.showLeftNavigation = false;
+
+    show();
+}
+
+function login() {
+    let username = document.getElementById('login-username').value;
+    let passwd = document.getElementById('login-passwd').value;
+    
+    if (username == '' || username.length < 3 || passwd == '' || passwd.length < 3) {
+        return;
+    }
+    
+    console.log(`Logged in as "${username}".`);
+
+    model.data.username = username;
+
+    showRealLandingPage();
 }
